83a753387c7b2d907a568c0795978a53975cc268
jasonlmfong <jasonlmfong@gmail.com>
2024-11-09 18:39:16 -0500

    flow of lights

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,28 @@ fn main() -> ! {
     // enable GPIOA clock
     rcc.apb2enr.modify(|_, w| w.iopaen().set_bit());
     // Configure PA0 as output (push-pull)
-    gpioa
-        .crl
-        .modify(|_, w| w.mode0().output().cnf0().push_pull());
+    gpioa.crl.modify(|_, w| {
+        w.mode0().output().cnf0().push_pull(); // PA0
+        w.mode1().output().cnf1().push_pull(); // PA1
+        w.mode2().output().cnf2().push_pull(); // PA2
+        w.mode3().output().cnf3().push_pull(); // PA3
+        w // return modified writer
+    });
 
     loop {
         gpioa.bsrr.write(|w| w.bs0().set_bit()); // set PA0 to high
-        cortex_m::asm::delay(10000000);
+        gpioa.bsrr.write(|w| w.br1().set_bit()); // set PA1 to low
+        cortex_m::asm::delay(2000000);
 
+        gpioa.bsrr.write(|w| w.bs1().set_bit()); // set PA1 to high
+        gpioa.bsrr.write(|w| w.br2().set_bit()); // set PA2 to low
+        cortex_m::asm::delay(2000000);
+
+        gpioa.bsrr.write(|w| w.bs2().set_bit()); // set PA2 to high
+        gpioa.bsrr.write(|w| w.br3().set_bit()); // set PA3 to low
+        cortex_m::asm::delay(2000000);
+
+        gpioa.bsrr.write(|w| w.bs3().set_bit()); // set PA3 to high
         gpioa.bsrr.write(|w| w.br0().set_bit()); // set PA0 to low
         cortex_m::asm::delay(2000000);
     }
