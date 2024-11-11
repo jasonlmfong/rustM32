0e447d93d252aefe5ebd00f040c557f509a31909
jasonlmfong <jasonlmfong@gmail.com>
2024-11-10 22:04:15 -0500

    button controlling LEDs program

--- src/main.rs
+++ src/main.rs
@@ -15,34 +15,57 @@ fn main() -> ! {
     // get handles to the hardware
     let peripherals = stm32f103::Peripherals::take().unwrap();
     let gpioa = &peripherals.GPIOA;
+    let gpiob = &peripherals.GPIOB;
     let rcc = &peripherals.RCC; // reset and clock control
 
-    // enable GPIOA clock
-    rcc.apb2enr.modify(|_, w| w.iopaen().set_bit());
-    // Configure PA0 as output (push-pull)
+    // enable GPIOA and GPIOB clocks
+    rcc.apb2enr.modify(|_, w| {
+        w.iopaen().set_bit();
+        w.iopben().set_bit();
+        w
+    });
+
+    // Configure PA0 and PA2 as output (push-pull)
     gpioa.crl.modify(|_, w| {
         w.mode0().output().cnf0().push_pull(); // PA0
-        w.mode1().output().cnf1().push_pull(); // PA1
         w.mode2().output().cnf2().push_pull(); // PA2
-        w.mode3().output().cnf3().push_pull(); // PA3
         w // return modified writer
     });
 
+    // Configure PB1 and PB11 as input
+    gpiob.crl.modify(|_, w| w.mode1().input().cnf1().bits(0b01));
+    gpiob
+        .crh
+        .modify(|_, w| w.mode11().input().cnf11().bits(0b01));
+
     loop {
-        gpioa.bsrr.write(|w| w.bs0().set_bit()); // set PA0 to high
-        gpioa.bsrr.write(|w| w.br1().set_bit()); // set PA1 to low
-        cortex_m::asm::delay(2000000);
+        // read PB1
+        if gpiob.idr.read().idr1().bit_is_clear() {
+            cortex_m::asm::delay(2000);
+            while gpiob.idr.read().idr1().bit_is_clear() {} // do nothing while button is held down
+            cortex_m::asm::delay(2000);
 
-        gpioa.bsrr.write(|w| w.bs1().set_bit()); // set PA1 to high
-        gpioa.bsrr.write(|w| w.br2().set_bit()); // set PA2 to low
-        cortex_m::asm::delay(2000000);
+            if gpioa.odr.read().odr0().bit_is_clear() {
+                // if PA0 is currently on
+                gpioa.bsrr.write(|w| w.bs0().set_bit()); // Set PA0 to high, turning it off
+            } else {
+                // if PA0 is currently of
+                gpioa.bsrr.write(|w| w.br0().set_bit()); // Set PA0 to low, turning it on
+            }
+        }
 
-        gpioa.bsrr.write(|w| w.bs2().set_bit()); // set PA2 to high
-        gpioa.bsrr.write(|w| w.br3().set_bit()); // set PA3 to low
-        cortex_m::asm::delay(2000000);
-
-        gpioa.bsrr.write(|w| w.bs3().set_bit()); // set PA3 to high
-        gpioa.bsrr.write(|w| w.br0().set_bit()); // set PA0 to low
-        cortex_m::asm::delay(2000000);
+        // read PB11
+        if gpiob.idr.read().idr11().bit_is_clear() {
+            cortex_m::asm::delay(2000);
+            while gpiob.idr.read().idr11().bit_is_clear() {} // do nothing while button is held down
+            cortex_m::asm::delay(2000);
+            if gpioa.odr.read().odr2().bit_is_clear() {
+                // if PA2 is currently on
+                gpioa.bsrr.write(|w| w.bs2().set_bit()); // Set PA2 to high, turning it off
+            } else {
+                // if PA2 is currently off
+                gpioa.bsrr.write(|w| w.br2().set_bit()); // Set PA2 to low, turning it on
+            }
+        }
     }
 }
